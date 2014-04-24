424072ed08fde984c82f8586add0e2f3254e561e
felipeYanagiya <felipe.yanagiya@gmail.com>
2014-04-24 19:03:00 -0300

    modifications on deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-cp -rf ear/target/blog.ear /home/java01/Desktop/jboss/server/default/deploy/
+cp -rf ear/target/blog.ear /opt/jboss-as-7.1.1.Final/standalone/deployments/
 echo "blog.ear atualizado no JBOSS"
