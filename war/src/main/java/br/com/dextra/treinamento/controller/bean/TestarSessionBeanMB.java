8f4df1968a66f1732a85c09e787b574b1ac4e38f
felipeYanagiya <felipe.yanagiya@gmail.com>
2014-04-24 18:50:03 -0300

    start project

---
+++ war/src/main/java/br/com/dextra/treinamento/controller/bean/TestarSessionBeanMB.java
@@ -0,0 +1,11 @@
+package br.com.dextra.treinamento.controller.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "testarSessionBeanMB")
+@RequestScoped
+public class TestarSessionBeanMB {
+
+
+}
